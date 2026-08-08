e24035379d1741ca2d7ddc85652c4b8ee377944b
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-08-08 11:19:43 +0530

    feat: add authentication middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const { protect } = require("../middleware/authMiddleware");
+
 const {registerUser,loginUser}=require("../controllers/authController");
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
+
+router.get("/me", protect, (req, res) => {
+    res.json({
+        user: req.user
+    });
+});
 
 
 
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (userId) => {
     return jwt.sign(
-        {userId},
+        { userId },
         process.env.JWT_SECRET,
-        {expiresIn:"7d"}
+        { expiresIn: "7d" }
 
     );
 };
