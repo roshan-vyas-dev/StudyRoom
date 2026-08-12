09686ec327b27bbc095d508e193a7797b15cd8ab
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-08-12 11:19:06 +0530

    feat: add room update API

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -75,6 +75,57 @@ const getOneRoom = async (req, res) => {
     }
 }
 
-module.exports = { createRoom, getRooms, getOneRoom };
+const updateRoom = async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const { title } = req.body;
+
+        if (!title) {
+            return res.status(400).json({
+                message: "Title is required"
+            });
+        }
+
+        const room = await Room.findById(id);
+
+        if (!room) {
+            return res.status(404).json({ message: "Room not found" })
+        }
+
+        if (!room.createdBy.equals(req.user._id)) {
+            return res.status(403).json({ message: "Not allowed to update this room" })
+
+        }
+
+        room.title = title;
+
+        await room.save();
+
+        await room.populate("createdBy", "username profilePic");
+
+        return res.status(200).json({
+            message: "Room title updated",
+            room
+        })
+
+    } catch (error) {
+        
+        if (error.name === "ValidationError") {
+            return res.status(400).json({
+                message: error.message
+            });
+        }
+
+        return res.status(500).json({
+            message: error.message
+        });
+
+    }
+}
+
+module.exports = { createRoom, getRooms, getOneRoom, updateRoom };
 
 
--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createRoom ,getRooms,getOneRoom} = require("../controllers/roomController");
+const { createRoom ,getRooms,getOneRoom,updateRoom} = require("../controllers/roomController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", protect, createRoom);
 router.get("/",protect,getRooms);
 router.get("/:id",protect,getOneRoom);
+router.patch("/:id",protect,updateRoom);
+
 
 
 module.exports = router;
