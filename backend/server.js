0a6aac65d966b50db420b237befe441b134a213a
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-08-13 20:26:39 +0530

    feat: add frontend and socket.io real-time communication

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
+
+
 const app = require("./app");
 const http = require("http");
 const connectDB = require("./config/db");
+
+const { Server } = require("socket.io");
 
 connectDB();
 
@@ -11,9 +15,31 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
 
-
 server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
+})
+
+// socket
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173"
+    }
+});
+
+
+
+
+io.on("connection", (socket) => {
+    console.log("User connected :", socket.id);
+
+    socket.on("join-room", (roomId) => {
+        socket.join(roomId);
+
+        console.log("User joined room:", roomId);
+
+        socket.to(roomId).emit("user-joined");
+    });
 
 })
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import socket from "./services/socket";
+
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>app</h1>
+
+    </div>
+
+  )
+}
+
+export default App
